700bd5c5215ec012560d2b2561fc5849c627c287
noertker <noertker@fb.com>
2023-04-11 07:57:02 -0700

    Allowed CORS Origins for Join Session API

--- server/session_manager/src/http_server.py
+++ server/session_manager/src/http_server.py
@@ -151,6 +151,11 @@ async def start_iot_async_server():
         -token: string - JWT token or placeholder returned from the API
 """
 class JoinSessionHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
     def post(self):
         try:
             data = json.loads(self.request.body)
